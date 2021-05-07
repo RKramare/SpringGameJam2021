37338468711dd629d1a794e86c9012e8e36b6539
Rasmus Winzell <raswi727@student.liu.se>
2021-05-07 21:22:59 +0200

    Fungerande frisbee

---
+++ frisbee/FrisbeeRigidBody.gd
@@ -0,0 +1,20 @@
+extends RigidBody
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_process_input(true);
+	pass # Replace with function body.
+
+func _input(event):
+	if (event.is_action_released("SHOOT")):
+		self.apply_impulse(Vector3(0,0,0), Vector3(35,10,0))
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
