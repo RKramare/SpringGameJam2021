4a81f99f6697fa3ff286ff947cec6ddbc6eddfd8
Rasmus Winzell <raswi727@student.liu.se>
2021-05-07 22:03:04 +0200

    finished game

--- frisbee/FrisbeeRigidBody.gd
+++ frisbee/FrisbeeRigidBody.gd
@@ -14,6 +14,8 @@ func _ready():
 func _input(event):
 	if (event.is_action_released("SHOOT")):
 		self.apply_impulse(Vector3(0,0,0), Vector3(35,10,0))
+	if (event.is_action_released("RESET")):
+		get_tree().reload_current_scene()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
