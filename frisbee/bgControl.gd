4dfc248263a18858a2639b31e9d58560b6c6398d
Rasmus Kramare <raskr891@student.liu.se>
2021-05-08 13:15:28 +0200

    Added world env and backgrounds

---
+++ frisbee/bgControl.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+func _ready():
+	var bg = get_node("bgSprite")
+	var viewportWidth = get_viewport().size.x
+	var viewportHeight = get_viewport().size.y
+	var scale = viewportWidth / bg.texture.get_size().x
+
+	bg.set_position(Vector2(viewportWidth/2, viewportHeight/2))
+	bg.set_scale(Vector2(scale, scale))
